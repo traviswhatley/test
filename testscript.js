9c7ef8194baf0d5f24c1b8040e9b75615775856a
traviswhatley <travis.whatley121@gmail.com>
2015-07-23 10:01:03 -0600

    Update testscript.js

--- testscript.js
+++ testscript.js
@@ -1,7 +1,7 @@
-document.all('btn-simple').onclick = doFunction;
-document.all('btn-simple').onclick = function () { alert('hahahaha'); };
+document.getElementById('btn-simple').onclick = doFunction;
+document.gelElementById('btn-simple').onclick = function () { alert('hahahaha'); };
 
-var el = document.all('btn-simple');
+var el = document.getElementById('btn-simple');
   if (el.addEventListener)
     el.addEventListener('click', doFunction, false);
   else if (el.attachEvent)
