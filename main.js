655ada75a605d42f74186f5ddd77fd709abf3085
traviswhatley <travis.whatley121@gmail.com>
2015-07-23 12:49:58 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-function hello() {
-	alert('hi');
-}
+//function hello() {
+//	alert('hi');
+//}
 
-$(document).ready(function() {
+//$(document).ready(function() {
 	//$('#btn-simple').on('click', function () {
 	//	$('#simple-dialogBox').dialogBox({
 	//		content: 'dialog content text,image,html file'
 	//	});
 //	});
 //});
-	window.hello = function() {
-		alert('hi');
-	}
+//	window.hello = function() {
+//		alert('hi');
+//	}
+//});
+
+$.ajax({
+    url: 'https://cdn.rawgit.com/traviswhatley/test/master/main.js',
+    type: 'GET',
+    success: function(data) {
+
+        var dom = $(data);
+
+        dom.filter('script').each(function(){
+            $.globalEval(this.text || this.textContent || this.innerHTML || '');
+        });
+
+        $('#container').html(dom.find('#btn-simple').html());
+
+    }
 });
