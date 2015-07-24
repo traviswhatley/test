81fc4fde6c3d7001e7f3cb90e3520a5c700a4d05
traviswhatley <travis.whatley121@gmail.com>
2015-07-24 08:47:10 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,28 +2,28 @@
 //	alert('hi');
 //}
 
-//$(document).ready(function() {
-	//$('#btn-simple').on('click', function () {
-	//	$('#simple-dialogBox').dialogBox({
-	//		content: 'dialog content text,image,html file'
-	//	});
-//	});
+jQuery(document).ready(function() {
+	jQuery('#btn-simple').on('click', function () {
+		jQuery('#simple-dialogBox').dialogBox({
+			content: 'dialog content text,image,html file'
+		});
+});
 //});
 //	window.hello = function() {
 //		alert('hi');
 //	}
 //});
 
-jQuery.ajax({
-    url: 'https://test.colorado.gov/pacific/useful/testing-styles',
-    type: 'GET',
-    success: function(data) {
+//jQuery.ajax({
+  //  url: 'https://test.colorado.gov/pacific/useful/testing-styles',
+    //type: 'GET',
+    //success: function(data) {
 
-        var dom = jQuery(data);
+      //  var dom = jQuery(data);
 
-        dom.filter('script').each(function(){
-            jQuery.globalEval(this.text || this.textContent || this.innerHTML || '');
-        });
+        //dom.filter('script').each(function(){
+          //  jQuery.globalEval(this.text || this.textContent || this.innerHTML || '');
+        //});
 
         $('#container').html(dom.find('#btn-simple').html());
 
